dcd21d5feb1311346b913c159ca1fb21943ac936
ompateldev <bhojaniom2005@gmail.com>
2024-08-29 13:14:13 +0530

    add img Extention

--- Code.php
+++ Code.php
@@ -22,20 +22,26 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $tmpName = $_FILES["imgfile"]["tmp_name"];
     $folder = ("uploads/product/" . $faileName);
 
-
-    $sql = "INSERT INTO `user data` (`img`,`name`,`email`,`gender`,`language`,`interest`) VALUES ('$folder','$name', '$email','$gender','$language','$string')";
-
-    $result = mysqli_query($conn, $sql);
-
-    if ($result) {
-        session_start();
-        move_uploaded_file($tmpName, $folder);
-        $_SESSION['status'] = "Your data insert success fully";
+    if (!array($file_Extention, $allowed_Extention)) {
+        $_SESSION['stutas'] = "You are allowed with only jpg , png , jpeg";
         header("location:index.php");
         exit();
     } else {
-        $_SESSION['status'] = "Your data insert not success fully";
-        header("location:index.php");
-        exit();
+        $sql = "INSERT INTO `user data` (`img`,`name`,`email`,`gender`,`language`,`interest`) 
+                                 VALUES ('$folder','$name', '$email','$gender','$language','$string')";
+        $result = mysqli_query($conn, $sql);
+
+        if ($result) {
+            session_start();
+            move_uploaded_file($tmpName, $folder);
+            $_SESSION['status'] = "Your data insert success fully";
+            header("location:index.php");
+            exit();
+        } else {
+            $_SESSION['status'] = "Your data insert not success fully";
+            header("location:index.php");
+            exit();
+        }
     }
+    
 }
